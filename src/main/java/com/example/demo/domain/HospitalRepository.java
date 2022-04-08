96ff9a3d527beba69afc628c26490ac99383f30b
Yongseok1991 <dobest0998@gmail.com>
2022-04-08 16:33:29 +0900

    조추첨

--- src/main/java/com/example/demo/domain/HospitalRepository.java
+++ src/main/java/com/example/demo/domain/HospitalRepository.java
@@ -15,13 +15,10 @@ public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
     @Query(value = "SELECT * FROM hospital WHERE sidoCdNm = :sidoCdNm AND sgguCdNm = :sgguCdNm AND pcrPsblYn = 'Y'", nativeQuery = true)
     public List<Hospital> mFindHospital(@Param("sidoCdNm") String sidoCdNm, @Param("sgguCdNm") String sgguCdNm);
 
-    // String도 됐었낭? 됨!! 저장하면 안됨..데이터 다시 받아야함 저장은 하지말장 - order by 추가
+
     @Query(value = "SELECT distinct sidoCdNm FROM hospital order by sidoCdNm", nativeQuery = true)
     public List<String> mFindSidoCdNm();
 
-    // 저장함 ㅠ 다시 배치 돌려야 할듯 // 대문자 안해서 오류났었네
-    // 콘솔 색깔좀 나오게 해야 겠당
-    // 대소문자좀 통일함. 해깔려서 통일을 해줘야함
     @Query(value = "SELECT distinct sgguCdNm FROM HOSPITAL WHERE sidoCdNm = :sidoCdNm order by sgguCdNm", nativeQuery = true)
     public List<String> mFindSggucdnm(@Param("sidoCdNm") String sidoCdNm);
 }
